e96f9790b35176c775891a09b1748c61fdf45608
kevin <kevinandres1250987@gmail.com>
2020-12-11 10:52:03 -0500

    base de datos

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-08-2020 a las 20:18:57
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.6
+-- Tiempo de generación: 11-12-2020 a las 15:10:53
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,7 +29,6 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `cotizacion` (
   `id_cotizacion` int(11) NOT NULL,
-  `numero_cotizacion` int(11) NOT NULL,
   `fecha` date NOT NULL,
   `nombre_cliente` varchar(40) NOT NULL,
   `email` varchar(30) DEFAULT NULL,
@@ -41,7 +40,8 @@ CREATE TABLE `cotizacion` (
   `tipo` varchar(30) DEFAULT NULL,
   `cantidad` int(11) DEFAULT NULL,
   `descripcion` varchar(500) DEFAULT NULL,
-  `total` decimal(10,0) NOT NULL
+  `total` int(11) NOT NULL,
+  `precio` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -55,7 +55,7 @@ CREATE TABLE `productos` (
   `nombre` varchar(25) NOT NULL,
   `codigo` int(11) NOT NULL,
   `unidades` int(11) NOT NULL,
-  `valor` decimal(10,0) NOT NULL,
+  `precio` int(11) NOT NULL,
   `referencia` varchar(30) DEFAULT NULL,
   `tipo` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -64,8 +64,8 @@ CREATE TABLE `productos` (
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id_producto`, `nombre`, `codigo`, `unidades`, `valor`, `referencia`, `tipo`) VALUES
-(1, 'foto', 1, 5, '1000', '34', 'producto');
+INSERT INTO `productos` (`id_producto`, `nombre`, `codigo`, `unidades`, `precio`, `referencia`, `tipo`) VALUES
+(1, 'foto', 1, -108, 1000, '34', 'producto');
 
 -- --------------------------------------------------------
 
@@ -93,7 +93,27 @@ CREATE TABLE `recibos` (
 --
 
 INSERT INTO `recibos` (`id_recibo`, `cajero`, `nombre_cliente`, `producto`, `cantidad`, `precio`, `subtotal`, `iva`, `valor_total`, `cantidad_pagada`, `cantidad_debida`, `fecha_reg`) VALUES
-(12, 'kevin', 'xiomar', 1, 2, 1000, 2000, 380, '2380', '2000', '0', '2020-08-10');
+(63, 'kevin', 'xiomara', 1, 2, 100000, 200000, 38000, '238000', '50000', '188000', '2020-11-25'),
+(66, 'kevin', 'alejandro', 1, 2, 40000, 80000, 15200, '95200', '50000', '45200', '2020-12-09');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `rol` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id`, `rol`) VALUES
+(1, 'admin'),
+(2, 'auxiliar');
 
 -- --------------------------------------------------------
 
@@ -104,16 +124,18 @@ INSERT INTO `recibos` (`id_recibo`, `cajero`, `nombre_cliente`, `producto`, `can
 CREATE TABLE `usuarios` (
   `id_usuario` int(11) NOT NULL,
   `nombre_usuario` varchar(20) NOT NULL,
-  `clave` varchar(10) NOT NULL
+  `clave` varchar(10) NOT NULL,
+  `rol_id` int(11) NOT NULL,
+  `correo` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `clave`) VALUES
-(3, 'kevin', '123'),
-(4, 'kevin', '123');
+INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `clave`, `rol_id`, `correo`) VALUES
+(6, 'kevin', '123', 1, 'kevinandres1250987@gmail.com'),
+(7, 'andres', '1234', 2, 'cristian.2604@hotmail.com');
 
 --
 -- Índices para tablas volcadas
@@ -141,10 +163,17 @@ ALTER TABLE `recibos`
   ADD KEY `fk_idproductorecibo` (`producto`);
 
 --
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id_usuario`);
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `usuarios_ibfk_1` (`rol_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -154,7 +183,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `cotizacion`
 --
 ALTER TABLE `cotizacion`
-  MODIFY `id_cotizacion` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cotizacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
@@ -166,13 +195,19 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `recibos`
 --
 ALTER TABLE `recibos`
-  MODIFY `id_recibo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_recibo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Restricciones para tablas volcadas
@@ -189,6 +224,12 @@ ALTER TABLE `cotizacion`
 --
 ALTER TABLE `recibos`
   ADD CONSTRAINT `fk_idproductorecibo` FOREIGN KEY (`producto`) REFERENCES `productos` (`id_producto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
